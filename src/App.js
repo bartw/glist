5bee4a8835a58029e4f9696982f4414da9f71c6f
Bart Wijnants <bart.w@sparkcentral.com>
2019-08-12 20:51:04 +0200

    lambda

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default () => (
-  <div>
-    <h1>Glist</h1>
-  </div>
-);
+export default () => {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    fetch("/.netlify/functions/hello")
+      .then(response => response.text())
+      .then(text => {
+        setMessage(text);
+      });
+  });
+
+  return (
+    <div>
+      <h1>Glist</h1>
+      {message && <h2>{message}</h2>}
+    </div>
+  );
+};
